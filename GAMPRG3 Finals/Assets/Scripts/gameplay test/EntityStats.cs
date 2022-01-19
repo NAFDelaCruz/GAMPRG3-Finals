48ec0dc1cfba7ea10697f2adb1f2b12314774c79
FSdelosreyes <fsdelosreyes@student.apc.edu.ph>
2022-01-19 22:28:28 +0800

    Priority system test
    Adds all entities to a list and then when pressing a button sorts them by their AGI attribute.

---
+++ GAMPRG3 Finals/Assets/Scripts/gameplay test/EntityStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityStats : MonoBehaviour
+{
+    //public int Level;
+    //public int HP;
+    //public int AP;
+    //public int STR;
+    //public int INT;
+    public int AGI;
+    //public int P_Def;
+    //public int M_Def;
+}
---
+++ GAMPRG3 Finals/Assets/Scripts/gameplay test/TurnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EntityPriority
+{
+    public GameObject Entity;
+    public int Entity_AGI;
+}
+
+public class TurnManager : MonoBehaviour
+{
+    public GameObject Entities;
+    public List<EntityPriority> EntityList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObtainEntities();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Sorting");
+            SortPriority();
+        }
+    }
+    void ObtainEntities()
+    {
+        for (int i = 0; i < Entities.transform.childCount; i++)
+        {
+            EntityList.Add(new EntityPriority() { Entity = Entities.transform.GetChild(i).gameObject, Entity_AGI = Entities.transform.GetChild(i).GetComponent<EntityStats>().AGI });
+        }
+    }
+
+    void SortPriority()
+    {
+        EntityList.Sort(SortByAGI);
+    }
+    static int SortByAGI(EntityPriority x, EntityPriority y)
+    {
+        return x.Entity_AGI.CompareTo(y.Entity_AGI);
+    }
+}
