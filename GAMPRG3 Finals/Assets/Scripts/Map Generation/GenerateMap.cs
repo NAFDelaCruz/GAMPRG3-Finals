e9c2b81b403fee1de702debf505e00de105703e8
Niel Dela Cruz <nielangelodelacruz@gmail.com>
2022-01-25 23:05:09 +0800

    Room-Like Map Generation Complete

--- GAMPRG3 Finals/Assets/Scripts/Map Generation/GenerateMap.cs
+++ GAMPRG3 Finals/Assets/Scripts/Map Generation/GenerateMap.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 //Special thanks to Danndx 2021 (youtube.com/danndx) for authoring the base code
 //From video: youtu.be/qNZ-0-7WuS8
 
-//This script has been modified by Niel Angelo Dela Cruz for the purpose of the game
+//This script has been heavily modified by Niel Angelo F. Dela Cruz for the purpose of the game
 
 public class GenerateMap : MonoBehaviour
 {
-
     //For each set create these
     [Header("CaveTiles")]
     [Tooltip("Set chances from highest to lowest.")]
@@ -22,11 +21,6 @@ public class GenerateMap : MonoBehaviour
     public int MapWidth;
     public int MapHeight;
 
-    [Header("Map Void Variables")]
-    public int VoidCount;
-    public int MinVoidSize;
-    public int MaxVoidSize;
-
     [Tooltip("Controls how detailed the map is. Recommended value: 4-20")]
     public float Magnification;
 
@@ -34,6 +28,20 @@ public class GenerateMap : MonoBehaviour
     public int XOffset = 0; // <- +>
     [Tooltip("Shifts tiles down (-) or up (+).")]
     public int YOffset = 0; // v- +^
+
+    [Header("Map Void Variables")]
+    [Tooltip("Recommened Values: 0-4.")]
+    public int VoidPerlinOctaves;
+    [Tooltip("Recommened Values: 0-2.")]
+    public float VoidLacunarity;
+    [Tooltip("Scale with Void Octaves, 4:100")]
+    public float VoidPerlinMagnification;
+    [Tooltip("Recommened Values: 0-3.5, the lower the value the lesser likely isolated islands spawn.")]
+    public float VoidPerlinSelector;
+    [Tooltip("Shifts tiles to the left (-) or right (+).")]
+    public int VoidPerlinXOffset;
+    [Tooltip("Shifts tiles down (-) or up (+).")]
+    public int VoidPerlinYOffset;
     
     List<List<float>> PrefabChances = new List<List<float>>();
     List<List<GameObject>> TilePrefabs = new List<List<GameObject>>();
@@ -48,7 +56,6 @@ public class GenerateMap : MonoBehaviour
 
         CreateTileSet();
         Generate();
-        //CreateVoids();
     }
 
     void CreateTileSet()
@@ -89,6 +96,8 @@ public class GenerateMap : MonoBehaviour
                 CreateTile(TilePrefab, x, y);
             }
         }
+
+        //CountVoids();
     }
 
     int GetIdUsingPerlin(int x, int y)
@@ -98,9 +107,10 @@ public class GenerateMap : MonoBehaviour
             to the number of tiles available. **/
 
         float RawPerlin = Mathf.PerlinNoise(
-            (x - XOffset) / Magnification,
+            (x - YOffset) / Magnification,
             (y - YOffset) / Magnification
         );
+
         float ClampPerlin = Mathf.Clamp01(RawPerlin); // Thanks: youtu.be/qNZ-0-7WuS8&lc=UgyoLWkYZxyp1nNc4f94AaABAg
         float ScaledPerlin = Mathf.Clamp(ClampPerlin * TileSets.Count, 0, TileSets.Count - 1);
 
@@ -111,15 +121,25 @@ public class GenerateMap : MonoBehaviour
     {
         /** Creates a new tile using the type id code, group it with common
             tiles, set it's position and store the gameobject. **/
-        
-        GameObject Tile = Instantiate(TilePrefab, gameObject.transform);
-        /*
-        Tiles TilesScript = TilePrefab.GetComponent<Tiles>();
-        TilesScript.XCoordinate = x;
-        TilesScript.YCoordinate = y;
-        */
-        Tile.name = string.Format("{0}{1}", x, y);
-        Tile.transform.localPosition = new Vector3(x, y, 0);
+        float VoidPerlin = new float();
+        float Frequency = 1;
+
+        for (int Index = 0; Index < VoidPerlinOctaves; Index++)
+        {
+            float X = (x - VoidPerlinXOffset) / VoidPerlinMagnification * Frequency;
+            float Y = (y - VoidPerlinXOffset) / VoidPerlinMagnification * Frequency;
+
+            VoidPerlin = Mathf.PerlinNoise(X, Y);
+
+            Frequency *= VoidLacunarity;
+        }
+
+        if (VoidPerlin > VoidPerlinSelector)
+        {
+            GameObject Tile = Instantiate(TilePrefab, gameObject.transform);
+            Tile.name = string.Format("{0}{1}", x, y);
+            Tile.transform.localPosition = new Vector3(x, y, 0);
+        }
     }
 
     GameObject SelectTile(Dictionary<float, GameObject> SelectedSet, float TotalWeight)
@@ -153,31 +173,5 @@ public class GenerateMap : MonoBehaviour
         }
 
         return TotalWeight;
-    }
-
-    void CountVoids()
-    {
-        for (int Index = 1; Index < VoidCount; Index++)
-        {
-
-            int X = Random.Range(0, MapWidth - 1);
-            int Y = Random.Range(0, MapHeight - 1);
-            int XVoidSize = Random.Range(MinVoidSize, MaxVoidSize);
-            int YVoidSize = Random.Range(MinVoidSize, MaxVoidSize);
-
-            int Size = YVoidSize;
-
-            if (XVoidSize > YVoidSize)
-                Size = XVoidSize;
-            CreateVoids(Size);
-        }
-    }
-
-    void CreateVoids(int Size)
-    {
-        for (int X = 0; X < Size; X++)
-        {
-               
-        }
     }
 }
