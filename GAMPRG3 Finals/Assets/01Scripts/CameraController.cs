8cc80588d23cc40524d040f42c89a464cbeea266
FSdelosreyes <fsdelosreyes@student.apc.edu.ph>
2022-02-03 16:53:48 +0800

    Camera controls and some script testing
    - Camera controls needs clamping
    - Investigate loading system
    - Added new folder for unit prefabs

---
+++ GAMPRG3 Finals/Assets/01Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject gameCamera;
+    public float MovementSensitivity;
+
+    public GameObject Map;
+    GenerateMap generateMap;
+    float xBorder;
+    float yBorder;
+
+    bool setToStart = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameCamera = this.gameObject;
+        generateMap = Map.GetComponent<GenerateMap>();
+        xBorder = generateMap.MapWidth;
+        yBorder = generateMap.MapHeight;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!setToStart)
+        {
+            gameCamera.transform.position = generateMap.StartPoint.transform.position + new Vector3(0, 0, -10);
+            if (gameCamera.transform.position.x == generateMap.StartPoint.transform.position.x)
+            {
+                setToStart = true;
+            }
+        }
+        //Using IJKL input system
+        if (Input.GetKey(KeyCode.I)) //Up
+        {
+            gameCamera.transform.position += new Vector3(0, MovementSensitivity * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.K)) //Down
+        {
+            gameCamera.transform.position -= new Vector3(0, MovementSensitivity * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.J)) //Left
+        {
+            gameCamera.transform.position -= new Vector3(MovementSensitivity * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.L)) //Right
+        {
+            gameCamera.transform.position += new Vector3(MovementSensitivity * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ GAMPRG3 Finals/Assets/01Scripts/Map Generation/UnitSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour
+{
+    public GameObject TestUnit;
+    public GameObject Map;
+    GenerateMap generateMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateMap = Map.GetComponent<GenerateMap>();
+        Instantiate(TestUnit, generateMap.StartPoint.transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
