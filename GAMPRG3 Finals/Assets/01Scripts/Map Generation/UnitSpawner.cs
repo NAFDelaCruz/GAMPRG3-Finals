07069aca2bfef3a90721e078052f6f13c960c7c1
Niel Dela Cruz <nielangelodelacruz@gmail.com>
2022-02-08 21:34:43 +0800

    Bug fix

--- GAMPRG3 Finals/Assets/01Scripts/Map Generation/UnitSpawner.cs
+++ GAMPRG3 Finals/Assets/01Scripts/Map Generation/UnitSpawner.cs
@@ -30,6 +30,7 @@ public class UnitSpawner : MonoBehaviour
         int TileIndex = 1;
         int UnitIndex = 0;
         
+        
         foreach (GameObject Unit in TestPartyUnits)
         {
             if (SpawnTiles.Count <= TileIndex)
@@ -37,27 +38,41 @@ public class UnitSpawner : MonoBehaviour
                 TileIndex = 1;
             }
 
-            if (!SpawnTiles[TileIndex].GetComponent<Tiles>().IsObstacle)
+            if (!SpawnTiles[TileIndex].GetComponent<Tiles>().IsObstacle && SpawnTiles[TileIndex].name != generateMap.StartPoint.name)
             {
                 Instantiate(TestPartyUnits[UnitIndex], SpawnTiles[TileIndex].gameObject.transform);
                 SpawnTiles.Remove(SpawnTiles[TileIndex]);
                 TileIndex++;
                 UnitIndex++;
             }
-            else if (SpawnTiles[TileIndex].GetComponent<Tiles>().IsObstacle && !SpawnTiles[0].GetComponent<Tiles>().IsObstacle)
+            else if (SpawnTiles[TileIndex].GetComponent<Tiles>().IsObstacle && !SpawnTiles[0].GetComponent<Tiles>().IsObstacle && SpawnTiles[TileIndex].name != "11")
             {
                 SpawnTiles.Remove(SpawnTiles[TileIndex]);
                 Instantiate(TestPartyUnits[UnitIndex], SpawnTiles[0].gameObject.transform);
                 SpawnTiles.Remove(SpawnTiles[0]);
                 UnitIndex++;
             }
-            else if (SpawnTiles[0].GetComponent<Tiles>().IsObstacle)
+            else if (SpawnTiles[0].GetComponent<Tiles>().IsObstacle && SpawnTiles[TileIndex].name != generateMap.StartPoint.name)
             {
                 SpawnTiles.Remove(SpawnTiles[0]);
                 SpawnTiles.Remove(SpawnTiles[1]);
                 Instantiate(TestPartyUnits[UnitIndex], SpawnTiles[TileIndex].gameObject.transform);
                 SpawnTiles.Remove(SpawnTiles[TileIndex]);
                 TileIndex++;
+                UnitIndex++;
+            }
+            else if (SpawnTiles[TileIndex].name == generateMap.StartPoint.name && !SpawnTiles[TileIndex + 1].GetComponent<Tiles>().IsObstacle)
+            {
+                Instantiate(TestPartyUnits[UnitIndex], SpawnTiles[TileIndex + 1].gameObject.transform);
+                SpawnTiles.Remove(SpawnTiles[TileIndex + 1]);
+                TileIndex++;
+                UnitIndex++;
+            } 
+            else if (SpawnTiles[TileIndex].name == generateMap.StartPoint.name && SpawnTiles[TileIndex + 1].GetComponent<Tiles>().IsObstacle)
+            {
+                Instantiate(TestPartyUnits[UnitIndex], SpawnTiles[TileIndex + 2].gameObject.transform);
+                SpawnTiles.Remove(SpawnTiles[TileIndex + 2]);
+                TileIndex += 2;
                 UnitIndex++;
             }
         }
