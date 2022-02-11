48697d18cfb9ae05a406774a5b064308a0fe9d33
Niel Dela Cruz <nielangelodelacruz@gmail.com>
2022-02-11 19:20:30 +0800

    Tile by Tile Movement (Marking) Complete

--- GAMPRG3 Finals/Assets/01Scripts/gameplay test/UnitActionController.cs
+++ GAMPRG3 Finals/Assets/01Scripts/gameplay test/UnitActionController.cs
@@ -8,13 +8,18 @@ public class UnitActionController : MonoBehaviour
 {
     public float AttackRange;
     public int ActionPoints;
-    private int CurrentActionPoints;
-    Color MoveColor = Color.green;
-    Color AttackColor = Color.red;
 
-    public Collider2D[] PossibleTiles;
+    int CurrentActionPoints;
+    int MoveIndex = -1;
+    string TileAction;
+    Color MoveColor = Color.blue;
+    Color AttackColor = Color.yellow;
+
+
+    public Collider2D[] CollidedTiles;
+    public List<GameObject> ValidTiles;
     public List<GameObject> SelectedTiles;
-    Dictionary<string, GameObject> TilePath;
+    public List<string> SelectedTileActions;
 
     private void Start()
     {
@@ -28,34 +33,57 @@ public class UnitActionController : MonoBehaviour
             Vector2 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(MousePosition, Vector2.zero);
 
-            if (PossibleTiles.Contains(hit.collider))
+            if (ValidTiles.Contains(hit.collider.gameObject))
             {
                 SelectedTiles.Add(hit.collider.gameObject);
+                SelectedTileActions.Add(TileAction);
+                if (TileAction == "Move")
+                {
+                    MoveIndex++;
+                    hit.collider.gameObject.GetComponent<SpriteRenderer>().color = MoveColor;
+                }
+                else if (TileAction == "Attack")
+                    hit.collider.gameObject.GetComponent<SpriteRenderer>().color = AttackColor;
+                ValidTiles.Remove(hit.collider.gameObject);
+                UnmarkTiles();
+                ValidTiles.Clear();
+                CurrentActionPoints--;
             }
         }
 
         if (Input.GetKeyDown(KeyCode.M))
         {
             GetMoveTiles();
+            TileAction = "Move";
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
             GetAttackTiles();
+            TileAction = "Attack";
         }
     }
 
     void ResetAP()
     {
         CurrentActionPoints = ActionPoints;
+        MoveIndex = -1;
     }
 
     public void GetMoveTiles()
     {
         if (CurrentActionPoints > 0)
         {
-            Debug.Log("Getting Move Tiles");
-            GetTiles(gameObject.transform, 1);
+            if (MoveIndex >= 0)
+            {
+                GetTiles(SelectedTiles[MoveIndex].transform, 1);
+                MarkTiles(MoveColor);
+            }
+            else if (MoveIndex < 0)
+            {
+                GetTiles(gameObject.transform, 1);
+                MarkTiles(MoveColor);
+            }
         }
     }
 
@@ -63,35 +91,49 @@ public class UnitActionController : MonoBehaviour
     {
         if (CurrentActionPoints > 0)
         {
-
-            Debug.Log("Getting Move Tiles");
-            GetTiles(gameObject.transform, AttackRange);
-            MarkTiles(AttackColor);
+            if (MoveIndex >= 0)
+            {
+                GetTiles(SelectedTiles[MoveIndex].transform, AttackRange);
+                MarkTiles(AttackColor);
+            }
+            else if (MoveIndex < 0)
+            {
+                GetTiles(gameObject.transform, AttackRange);
+                MarkTiles(AttackColor);
+            }  
         }
     }
 
     public void MarkTiles(Color TileColor)
     {
-        foreach (Collider2D tile in PossibleTiles)
+        foreach (GameObject tile in ValidTiles)
         {
-            tile.gameObject.GetComponent<SpriteRenderer>().color = TileColor;
+            if (!tile.GetComponent<Tiles>().IsObstacle)
+                tile.GetComponent<SpriteRenderer>().color = TileColor;
         }
 
     }
 
     public void UnmarkTiles()
     {
-        foreach (Collider2D tile in PossibleTiles)
+        foreach (GameObject tile in ValidTiles)
         {
-            tile.gameObject.GetComponent<SpriteRenderer>().color = Color.clear;
+            if (!tile.GetComponent<Tiles>().IsObstacle)
+                tile.GetComponent<SpriteRenderer>().color = Color.white;
         }
     }
 
     public virtual void GetTiles(Transform Tile, float Range)
     {
-        if (PossibleTiles.Length != 0)
-            Array.Clear(PossibleTiles, 0, PossibleTiles.Length);
+        if (CollidedTiles.Length != 0)
+            Array.Clear(CollidedTiles, 0, CollidedTiles.Length);
 
-        PossibleTiles = Physics2D.OverlapBoxAll(new Vector2(Tile.position.x, Tile.position.y), new Vector2(Range, Range), 0);
+        CollidedTiles = Physics2D.OverlapBoxAll(new Vector2(Tile.position.x, Tile.position.y), new Vector2(Range, Range), 0);
+
+        foreach (Collider2D tile in CollidedTiles)
+        {
+            if (!tile.gameObject.GetComponent<Tiles>().IsObstacle && tile.gameObject.GetComponent<SpriteRenderer>().color == Color.white)
+                ValidTiles.Add(tile.gameObject);
+        }
     }
 }
