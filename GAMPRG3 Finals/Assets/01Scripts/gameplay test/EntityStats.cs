2320e0c20389ad03de1c82fe2613fea551dd4bef
FSdelosreyes <fsdelosreyes@student.apc.edu.ph>
2022-02-15 04:01:19 +0800

    formulas

--- GAMPRG3 Finals/Assets/01Scripts/gameplay test/EntityStats.cs
+++ GAMPRG3 Finals/Assets/01Scripts/gameplay test/EntityStats.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class EntityStats : MonoBehaviour
 {
-    //public int Level;
+    public int Level;
+    public int EXP;
+
     public int HP;
-    //public int AP;
-    //public int STR;
-    //public int INT;
+    public int AP;
+
     public int AGI;
-    //public int P_Def;
-    //public int M_Def;
+
+    public int STR; //Physical Damage
+    public int INT; //Magic Damage
+
+    public int DEF; //Physical Resistance
+    public int CON; //Magical Resistance
 }
