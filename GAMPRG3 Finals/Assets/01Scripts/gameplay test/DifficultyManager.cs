4e72c961e27b960f1cdf75d4598244b8ca75d106
FSdelosreyes <fsdelosreyes@student.apc.edu.ph>
2022-02-20 11:13:58 +0800

    SpawningLevelScript and Difficulty Script

---
+++ GAMPRG3 Finals/Assets/01Scripts/gameplay test/DifficultyManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour
+{
+    public int Stage; 
+    //To be used for enemy stat generation and for map size variability
+    //Increase through events? or directly when a stage is finished
+}
---
+++ GAMPRG3 Finals/Assets/01Scripts/gameplay test/SpawnLevelScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLevelScript : MonoBehaviour
+{
+
+    //To be attached to enemy prefabs
+    //will set a predetermined random level based on what stage the player is on
+    //Calls the level up function until the current level is set (for the purpose of random stats)
+    //When the set level is reached the script is destroyed and the enemy will act as normal
+
+    public LevellingScript levellingScript;
+    public DifficultyManager difficultyManager;
+    public EntityStats entityStats;
+
+    int currentLevel;
+    int setLevel;
+    private void Start()
+    {
+        entityStats = this.gameObject.GetComponent<EntityStats>();
+        levellingScript = this.gameObject.GetComponent<LevellingScript>();
+        difficultyManager = this.gameObject.GetComponent<DifficultyManager>();
+
+        entityStats.Level = 0;
+    }
+
+    private void Update()
+    {
+        if (currentLevel <= setLevel)
+        {
+            //Call Level Up Function in levelling script
+        }
+        else
+        {
+            Destroy(this); //Destroy this script
+        }
+    }
+
+    private void SetNewLevel()
+    {
+        setLevel = Mathf.FloorToInt(Random.Range(difficultyManager.Stage,3+difficultyManager.Stage));
+    }
+}
