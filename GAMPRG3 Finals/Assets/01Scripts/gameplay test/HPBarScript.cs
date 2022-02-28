8f9b6b07e23a7dc410c171dc8082b3578a3b9c7b
FSdelosreyes <fsdelosreyes@student.apc.edu.ph>
2022-02-28 22:48:32 +0800

    HP Bar and Box Colliders
    HP Script attached
    Added an HP Bar canvas prefab under 02TilePrefabs\UI
    Box Colliders added to all unit prefabs (friendly & enemy)

---
+++ GAMPRG3 Finals/Assets/01Scripts/gameplay test/HPBarScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarScript : MonoBehaviour
+{
+    public float fill_perc;
+
+    public EntityStats entityStats;
+
+    public GameObject Fill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entityStats = this.GetComponentInParent<EntityStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fill_perc = (float)entityStats.Curr_HP / (float)entityStats.HP;
+        Fill.GetComponent<Image>().fillAmount = fill_perc;
+    }
+}
